d0ab5a3c9a38388e588be0bd3e18a361a77211e3
SebastienPingal <sebastien.pingal@gmail.com>
2022-08-31 14:52:01 +0200

    corrected
    Co-authored-by: white_rqbbit <whiterqbbit@users.noreply.github.com>

--- back-end/src/Logic/checkers.js
+++ back-end/src/Logic/checkers.js
@@ -1,87 +1,78 @@
-function test_json(leJson){
-    let statusCode = []
-    if (check_json_lvl(leJson) > 3 ){
-        statusCode.push(1);
-    };
-    if (check_json_contain_array(leJson).length != 0){
-        statusCode.push(2);
-    }
-    if (check_processing_error(leJson) != 0){
+function test_json(leJson) {
+    let statusCode = [];
+    let maxLvlAllowed = 3;
+    if (check_json_lvl(leJson) > maxLvlAllowed) statusCode.push(1);
+    if (check_json_contain_array(leJson).length != 0) statusCode.push(2);
+
+    if (check_processing_error(leJson) != 0) {
         statusCode.push(3);
         const temp = check_processing_error(leJson);
-        statusCode.push(temp.length)
-        for (const t in temp){
-        statusCode.push(temp[t]);
+        statusCode.push(temp.length);
+        for (const t in temp) statusCode.push(temp[t]);
     }
-    }
-    if (check_json_less_than_64_char(leJson) != 0){
-        statusCode.push(4);
-    }
-    return statusCode;
-}  
+    if (check_json_less_than_64_char(leJson) != 0) statusCode.push(4);
 
-function interprete_error(lArray){
-    let log_error = "";
-    for (let t in lArray){
-        switch (lArray[t]){
-            case 1 :
-                log_error += "Json file too deep." + "\n";
-                break;
-            case 2 :
-                log_error += "Json file contain array." + "\n";
-                break;
-            case 3 :
-                for (let i=2; i <= lArray[Number(t)+1]+1; i++){
-                log_error += "Processing error at " + lArray[Number(t)+ i] + "\n";
-                }
-                break;
-            case 4 :
-                log_error += "At least one key is too long"+ "\n";
-                break;
-        }        
-    }
-    if (log_error == ""){
-        console.log("Valid Json")
-    }
-    else {
-        console.log(log_error);
-    }
+    return statusCode;
 }
 
-function is_json_Ok(leJson){
+function interprete_error(lArray) {
+    let log_error = "";
+    for (let t in lArray) {
+        switch (lArray[t]) {
+            case 1:
+                log_error += "Json file too deep." + "\n";
+                break;
+            case 2:
+                log_error += "Json file contain array." + "\n";
+                break;
+            case 3:
+                for (let i = 2; i <= lArray[Number(t) + 1] + 1; i++)
+                    log_error +=
+                        "Processing error at " + lArray[Number(t) + i] + "\n";
+                break;
+            case 4:
+                log_error += "At least one key is too long" + "\n";
+                break;
+            default:
+                log_error += "Erreur inconnue";
+        }
+    }
+    if (log_error == "") console.log("Valid Json");
+    else console.log(log_error);
+}
+
+function is_json_Ok(leJson) {
     return interprete_error(test_json(leJson));
 }
 
-function check_json_lvl(leJson){
+function check_json_lvl(leJson) {
     const name = Object.keys(leJson);
     let lvlMax = 1;
     for (const property in name) {
-        if (typeof leJson[name[property]] == "object"){
+        if (typeof leJson[name[property]] == "object") {
             let lvl = 1;
             lvl += check_json_lvl(leJson[name[property]]);
-            if (lvl > lvlMax){
-            lvlMax = lvl;
-            }
+            if (lvl > lvlMax) lvlMax = lvl;
         }
-      }
+    }
     return lvlMax;
 }
 
-function check_json_contain_array(leJson){
+function check_json_contain_array(leJson) {
     const name = Object.keys(leJson);
     let errors = [];
     for (const property in name) {
-        if (Array.isArray(leJson[name[property]])){
+        if (Array.isArray(leJson[name[property]])) {
             errors.push(name[property]);
             return errors;
         }
-        if (typeof leJson == "object"){
-            const t = check_json_contain_array(leJson[name[property]])
-            if (t != [] ){
-            errors.push(t);
+        if (typeof leJson == "object") {
+            const t = check_json_contain_array(leJson[name[property]]);
+            if (t != []) {
+                errors.push(t);
             }
         }
-      }
+    }
     return errors.flat();
 }
 
@@ -89,198 +80,195 @@ function check_processing_error(leJson) {
     const name = Object.keys(leJson);
     let errors = [];
     for (const property in name) {
-        if (leJson[name[property]] == "#ERROR PROCESSING"){
+        if (leJson[name[property]] == "#ERROR PROCESSING") {
             errors.push(name[property]);
             return errors;
         }
-        if (typeof leJson == "object"){
+        if (typeof leJson == "object") {
             //console.log ("boucle " + leJson[name[property]]);
-            const t = check_processing_error(leJson[name[property]])
-            if (t != 0 ){
-            errors.push(t);
-            }
+            const t = check_processing_error(leJson[name[property]]);
+            if (t != 0) errors.push(t);
         }
-      }
+    }
     return errors.flat();
 }
 
-function check_json_less_than_64_char(leJson){
+function check_json_less_than_64_char(leJson) {
     const name = Object.keys(leJson);
     for (const property in name) {
         //console.log(leJson[name[property]].length);
-        if (typeof name[property] == "string" && name[property].length > 64){
+        if (typeof name[property] == "string" && name[property].length > 64) {
             return "error";
         }
-        if (typeof leJson == "object"){
+        if (typeof leJson == "object") {
             //console.log ("boucle " + leJson[name[property]]);
             const nest = check_json_less_than_64_char(leJson[name[property]]);
-            if (nest != 0){
-                return nest;
-            }
+            if (nest != 0) return nest;
         }
-      }
+    }
     return "0";
 }
 
 const json_error = {
-    "1": {
-        "id": "001",
-        "customer": "Mickael",
-        "items": [7],
-        "contact": {
-            "mail": "mickael@gmail.com",
-            "phone": "0123456789"
-        }
+    1: {
+        id: "001",
+        customer: "Mickael",
+        items: [7],
+        contact: {
+            mail: "mickael@gmail.com",
+            phone: "0123456789",
+        },
     },
-    "2": {
-        "id": "002",
-        "customer": "sarah",
-        "items": 2,
-        "contact": {
-            "mail": "sarah@gmail.com",
-            "phone": "0234567891"
-        }
+    2: {
+        id: "002",
+        customer: "sarah",
+        items: 2,
+        contact: {
+            mail: "sarah@gmail.com",
+            phone: "0234567891",
+        },
     },
-    "3": {
-        "id": "003",
-        "customer": "Joseph",
-        "items": 1,
-        "contact": {
-            "mail": ["joseph@gmail.com"],
-            "phone": "0345678912"
-        }
+    3: {
+        id: "003",
+        customer: "Joseph",
+        items: 1,
+        contact: {
+            mail: ["joseph@gmail.com"],
+            phone: "0345678912",
+        },
     },
-    "4": {
-        "id": "004",
-        "customer": "François",
-        "items": 67,
-        "contact": {
-            "mail": "francois@gmail.com",
-            "phone": "0456789123"
-        }
-    }
-}
+    4: {
+        id: "004",
+        customer: "François",
+        items: 67,
+        contact: {
+            mail: "francois@gmail.com",
+            phone: "0456789123",
+        },
+    },
+};
 
 const json_error2 = {
-    "1": {
-        "id": "001",
-        "customer": "Mickael",
-        "items": 7,
-        "contact": {
-            "mail": "mickael@gmail.com",
-            "phone": "0123456789"
-        }
+    1: {
+        id: "001",
+        customer: "Mickael",
+        items: 7,
+        contact: {
+            mail: "mickael@gmail.com",
+            phone: "0123456789",
+        },
     },
-    "2": {
-        "id": "002",
-        "customer": "#ERROR PROCESSING",
-        "items": 2,
-        "contact": {
-            "mail": "sarah@gmail.com",
-            "phone": "0234567891"
-        }
+    2: {
+        id: "002",
+        customer: "#ERROR PROCESSING",
+        items: 2,
+        contact: {
+            mail: "sarah@gmail.com",
+            phone: "0234567891",
+        },
     },
-    "3": {
-        "id": "003",
-        "customer": "Joseph",
-        "items": 1,
-        "contact": {
-            "mail": "joseph@gmail.com",
-            "phone": "0345678912"
-        }
+    3: {
+        id: "003",
+        customer: "Joseph",
+        items: 1,
+        contact: {
+            mail: "joseph@gmail.com",
+            phone: "0345678912",
+        },
     },
-    "4": {
-        "id": "004",
-        "customer": "François",
-        "items": 67,
-        "contact": {
-            "mail": "francois@gmail.com",
-            "phone": "#ERROR PROCESSING"
-        }
-    }
-}
+    4: {
+        id: "004",
+        customer: "François",
+        items: 67,
+        contact: {
+            mail: "francois@gmail.com",
+            phone: "#ERROR PROCESSING",
+        },
+    },
+};
 
 const json_error3 = {
-    "1": {
-        "id": "001",
-        "customer": "Mickael",
-        "items": 7,
-        "contact": {
-            "mail": "mickael@gmail.com",
-            "phone": "0123456789"
-        }
+    1: {
+        id: "001",
+        customer: "Mickael",
+        items: 7,
+        contact: {
+            mail: "mickael@gmail.com",
+            phone: "0123456789",
+        },
     },
-    "2": {
-        "id": "002",
-        "customer": "sarah",
-        "items": 2,
-        "contact": {
-            "mail": "sarah@gmail.com",
-            "phone": "0234567891"
-        }
+    2: {
+        id: "002",
+        customer: "sarah",
+        items: 2,
+        contact: {
+            mail: "sarah@gmail.com",
+            phone: "0234567891",
+        },
     },
-    "3": {
-        "id": "003",
-        "customer": "Joseph",
-        "items": 1,
-        "longcat_is_loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong": {
-            "mail": "joseph@gmail.com",
-            "phone": "0345678912"
-        }
+    3: {
+        id: "003",
+        customer: "Joseph",
+        items: 1,
+        longcat_is_loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong:
+            {
+                mail: "joseph@gmail.com",
+                phone: "0345678912",
+            },
     },
-    "4": {
-        "id": "004",
-        "customer": "François",
+    4: {
+        id: "004",
+        customer: "François",
         "items are soooooo boriiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiing omg": 67,
-        "contact": {
-            "mail": "francois@gmail.com",
-            "phone": "0456789123"
-        }
-    }
-}
+        contact: {
+            mail: "francois@gmail.com",
+            phone: "0456789123",
+        },
+    },
+};
 
 const json_valid = [
     {
-        "name": "John Smith",
-        "sku": "20223",
-        "price": 23.95,
-        "shipTo": {
-            "name": "Jane Smith",
-            "address": "123 Maple Street",
-            "city": "Pretendville",
-            "state": "NY",
-            "zip": "12345"
+        name: "John Smith",
+        sku: "20223",
+        price: 23.95,
+        shipTo: {
+            name: "Jane Smith",
+            address: "123 Maple Street",
+            city: "Pretendville",
+            state: "NY",
+            zip: "12345",
         },
-        "billTo": {
-            "name": "John Smith",
-            "address": "123 Maple Street",
-            "city": "Pretendville",
-            "state": "NY",
-            "zip": "12345"
-        }
+        billTo: {
+            name: "John Smith",
+            address: "123 Maple Street",
+            city: "Pretendville",
+            state: "NY",
+            zip: "12345",
+        },
     },
     {
-        "name": "Alice Brown",
-        "sku": "54321",
-        "price": 199.95,
-        "shipTo": {
-            "name": "Bob Brown",
-            "address": "456 Oak Lane",
-            "city": "Pretendville",
-            "state": "HI",
-            "zip": "98999"
+        name: "Alice Brown",
+        sku: "54321",
+        price: 199.95,
+        shipTo: {
+            name: "Bob Brown",
+            address: "456 Oak Lane",
+            city: "Pretendville",
+            state: "HI",
+            zip: "98999",
         },
-        "billTo": {
-            "name": "Alice Brown",
-            "address": "456 Oak Lane",
-            "city": "Pretendville",
-            "state": "HI",
-            "zip": "98999"
-        }
-    }
-]
+        billTo: {
+            name: "Alice Brown",
+            address: "456 Oak Lane",
+            city: "Pretendville",
+            state: "HI",
+            zip: "98999",
+        },
+    },
+];
 
-const jsonsToTest = [json_error, json_error2, json_error3, json_valid ]
+const jsonsToTest = [json_error, json_error2, json_error3, json_valid];
 
 /*
 for (const element in jsonsToTest){
--- back-end/src/Logic/converters.js
+++ back-end/src/Logic/converters.js
@@ -1,310 +1,312 @@
 const json_error = {
-    "1": {
-        "id": "001",
-        "customer": "Mickael",
-        "items": [7],
-        "contact": {
-            "mail": "mickael@gmail.com",
-            "phone": "0123456789"
-        }
+    1: {
+        id: "001",
+        customer: "Mickael",
+        items: [7],
+        contact: {
+            mail: "mickael@gmail.com",
+            phone: "0123456789",
+        },
     },
-    "2": {
-        "id": "002",
-        "customer": "sarah",
-        "items": 2,
-        "contact": {
-            "mail": "sarah@gmail.com",
-            "phone": "0234567891"
-        }
+    2: {
+        id: "002",
+        customer: "sarah",
+        items: 2,
+        contact: {
+            mail: "sarah@gmail.com",
+            phone: "0234567891",
+        },
     },
-    "3": {
-        "id": "003",
-        "customer": "Joseph",
-        "items": 1,
-        "contact": {
-            "mail": ["joseph@gmail.com"],
-            "phone": "0345678912"
-        }
+    3: {
+        id: "003",
+        customer: "Joseph",
+        items: 1,
+        contact: {
+            mail: ["joseph@gmail.com"],
+            phone: "0345678912",
+        },
     },
-    "4": {
-        "id": "004",
-        "customer": "François",
-        "items": 67,
-        "contact": {
-            "mail": "francois@gmail.com",
-            "phone": "0456789123"
-        }
-    }
-}
+    4: {
+        id: "004",
+        customer: "François",
+        items: 67,
+        contact: {
+            mail: "francois@gmail.com",
+            phone: "0456789123",
+        },
+    },
+};
 
 const json_error2 = {
-    "1": {
-        "id": "001",
-        "customer": "Mickael",
-        "items": 7,
-        "contact": {
-            "mail": "mickael@gmail.com",
-            "phone": "0123456789"
-        }
+    1: {
+        id: "001",
+        customer: "Mickael",
+        items: 7,
+        contact: {
+            mail: "mickael@gmail.com",
+            phone: "0123456789",
+        },
     },
-    "2": {
-        "id": "002",
-        "customer": "#ERROR PROCESSING",
-        "items": 2,
-        "contact": {
-            "mail": "sarah@gmail.com",
-            "phone": "0234567891"
-        }
+    2: {
+        id: "002",
+        customer: "#ERROR PROCESSING",
+        items: 2,
+        contact: {
+            mail: "sarah@gmail.com",
+            phone: "0234567891",
+        },
     },
-    "3": {
-        "id": "003",
-        "customer": "Joseph",
-        "items": 1,
-        "contact": {
-            "mail": "joseph@gmail.com",
-            "phone": "0345678912"
-        }
+    3: {
+        id: "003",
+        customer: "Joseph",
+        items: 1,
+        contact: {
+            mail: "joseph@gmail.com",
+            phone: "0345678912",
+        },
     },
-    "4": {
-        "id": "004",
-        "customer": "François",
-        "items": 67,
-        "contact": {
-            "mail": "francois@gmail.com",
-            "phone": "#ERROR PROCESSING"
-        }
-    }
-}
+    4: {
+        id: "004",
+        customer: "François",
+        items: 67,
+        contact: {
+            mail: "francois@gmail.com",
+            phone: "#ERROR PROCESSING",
+        },
+    },
+};
 
 const json_error3 = {
-    "1": {
-        "id": "001",
-        "customer": "Mickael",
-        "items": 7,
-        "contact": {
-            "mail": "mickael@gmail.com",
-            "phone": "0123456789"
-        }
+    1: {
+        id: "001",
+        customer: "Mickael",
+        items: 7,
+        contact: {
+            mail: "mickael@gmail.com",
+            phone: "0123456789",
+        },
     },
-    "2": {
-        "id": "002",
-        "customer": "sarah",
-        "items": 2,
-        "contact": {
-            "mail": "sarah@gmail.com",
-            "phone": "0234567891"
-        }
+    2: {
+        id: "002",
+        customer: "sarah",
+        items: 2,
+        contact: {
+            mail: "sarah@gmail.com",
+            phone: "0234567891",
+        },
     },
-    "3": {
-        "id": "003",
-        "customer": "Joseph",
-        "items": 1,
-        "longcat_is_loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong": {
-            "mail": "joseph@gmail.com",
-            "phone": "0345678912"
-        }
+    3: {
+        id: "003",
+        customer: "Joseph",
+        items: 1,
+        longcat_is_loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong:
+            {
+                mail: "joseph@gmail.com",
+                phone: "0345678912",
+            },
     },
-    "4": {
-        "id": "004",
-        "customer": "François",
+    4: {
+        id: "004",
+        customer: "François",
         "items are soooooo boriiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiing omg": 67,
-        "contact": {
-            "mail": "francois@gmail.com",
-            "phone": "0456789123"
-        }
-    }
-}
+        contact: {
+            mail: "francois@gmail.com",
+            phone: "0456789123",
+        },
+    },
+};
 
 const json_valid = [
     {
-        "name": "John Smith",
-        "sku": "20223",
-        "price": 23.95,
-        "shipTo": {
-            "name": "Jane Smith",
-            "address": "123 Maple Street",
-            "city": "Pretendville",
-            "state": "NY",
-            "zip": "12345"
+        name: "John Smith",
+        sku: "20223",
+        price: 23.95,
+        shipTo: {
+            name: "Jane Smith",
+            address: "123 Maple Street",
+            city: "Pretendville",
+            state: "NY",
+            zip: "12345",
         },
-        "billTo": {
-            "name": "John Smith",
-            "address": "123 Maple Street",
-            "city": "Pretendville",
-            "state": "NY",
-            "zip": "12345"
-        }
+        billTo: {
+            name: "John Smith",
+            address: "123 Maple Street",
+            city: "Pretendville",
+            state: "NY",
+            zip: "12345",
+        },
     },
     {
-        "name": "Alice Brown",
-        "sku": "54321",
-        "price": 199.95,
-        "shipTo": {
-            "name": "Bob Brown",
-            "address": "456 Oak Lane",
-            "city": "Pretendville",
-            "state": "HI",
-            "zip": "98999"
+        name: "Alice Brown",
+        sku: "54321",
+        price: 199.95,
+        shipTo: {
+            name: "Bob Brown",
+            address: "456 Oak Lane",
+            city: "Pretendville",
+            state: "HI",
+            zip: "98999",
         },
-        "billTo": {
-            "name": "Alice Brown",
-            "address": "456 Oak Lane",
-            "city": "Pretendville",
-            "state": "HI",
-            "zip": "98999"
-        }
-    }
-]
+        billTo: {
+            name: "Alice Brown",
+            address: "456 Oak Lane",
+            city: "Pretendville",
+            state: "HI",
+            zip: "98999",
+        },
+    },
+];
 
 const json_valid2 = [
     {
-        "color": "red",
-        "value": "#f00"
+        color: "red",
+        value: "#f00",
     },
     {
-        "color": "green",
-        "value": "#0f0"
+        color: "green",
+        value: "#0f0",
     },
     {
-        "color": "blue",
-        "value": "#00f"
+        color: "blue",
+        value: "#00f",
     },
     {
-        "color": "cyan",
-        "value": "#0ff"
+        color: "cyan",
+        value: "#0ff",
     },
     {
-        "color": "magenta",
-        "value": "#f0f"
+        color: "magenta",
+        value: "#f0f",
     },
     {
-        "color": "yellow",
-        "value": "#ff0"
+        color: "yellow",
+        value: "#ff0",
     },
     {
-        "color": "black",
-        "value": "#000"
-    }
-]
+        color: "black",
+        value: "#000",
+    },
+];
 
 const json_valid3 = {
-    "1": {
-        "id": "001",
-        "customer": "Mickael",
-        "items": 7,
-        "contact": {
-            "mail": "mickael@gmail.com",
-            "phone": "0123456789"
-        }
+    1: {
+        id: "001",
+        customer: "Mickael",
+        items: 7,
+        contact: {
+            mail: "mickael@gmail.com",
+            phone: "0123456789",
+        },
     },
-    "2": {
-        "id": "002",
-        "customer": "sarah",
-        "items": 2,
-        "contact": {
-            "mail": "sarah@gmail.com",
-            "phone": "0234567891"
-        }
+    2: {
+        id: "002",
+        customer: "sarah",
+        items: 2,
+        contact: {
+            mail: "sarah@gmail.com",
+            phone: "0234567891",
+        },
     },
-    "3": {
-        "id": "003",
-        "customer": "Joseph",
-        "items": 1,
-        "contact": {
-            "mail": "joseph@gmail.com",
-            "phone": "0345678912"
-        }
+    3: {
+        id: "003",
+        customer: "Joseph",
+        items: 1,
+        contact: {
+            mail: "joseph@gmail.com",
+            phone: "0345678912",
+        },
     },
-    "4": {
-        "id": "004",
-        "customer": "François",
-        "items": 67,
-        "contact": {
-            "mail": "francois@gmail.com",
-            "phone": "0456789123"
-        }
-    }
-}
+    4: {
+        id: "004",
+        customer: "François",
+        items: 67,
+        contact: {
+            mail: "francois@gmail.com",
+            phone: "0456789123",
+        },
+    },
+};
 
-const csv_valid = ""
+const csv_valid = "";
 
-const jsonsToConvert = [json_error, json_error2, json_error3, json_valid ]
+const jsonsToConvert = [json_error, json_error2, json_error3, json_valid];
 
-function json_to_csv (leJson){
-    const sprt      = " ; " ;
-    let header      = []    ;
-    let content     = []    ;
+function json_to_csv(leJson) {
+    const sprt = " ; ";
+    let header = [];
+    let content = [];
 
-    for (const i in leJson){
+    for (const i in leJson) {
         let content_line = [];
-        for (const [key, value] of Object.entries(leJson[i])){
-                if (typeof value == "object"){
-                    for(const [k, v] of Object.entries(leJson[i][key])){
-                        if (header.includes(key + "_" + k) == false){
-                            header.push(key + "_" + k, sprt);
-                        }
-                        content_line[header.indexOf(key + "_" + k)] = v;
-                        content_line[header.indexOf(key + "_" + k)+1] = sprt;
+        for (const [key, value] of Object.entries(leJson[i])) {
+            if (typeof value == "object") {
+                for (const [k, v] of Object.entries(leJson[i][key])) {
+                    if (header.includes(key + "_" + k) == false) {
+                        header.push(key + "_" + k, sprt);
                     }
+                    content_line[header.indexOf(key + "_" + k)] = v;
+                    content_line[header.indexOf(key + "_" + k) + 1] = sprt;
                 }
-                else {
-                    if (header.includes(key) == false){
-                        header.push(key, sprt);
-                    }
-                    content_line[header.indexOf(key)]= value;
-                    content_line[header.indexOf(key)+1]= sprt;
+            } else {
+                if (header.includes(key) == false) {
+                    header.push(key, sprt);
                 }
+                content_line[header.indexOf(key)] = value;
+                content_line[header.indexOf(key) + 1] = sprt;
+            }
         }
-        content_line.splice(-1,1);    
-        
+        content_line.splice(-1, 1);
+
         content.push(content_line);
     }
 
-    header.splice(-1,1);
+    header.splice(-1, 1);
 
     let csv = "";
 
-    for (const i in header){
-        csv += header [i];
+    for (const i in header) {
+        csv += header[i];
     }
 
     csv += "\n";
 
-    for (const i in content){
-        for (const j in content[i]){
-            csv += content[i][j]
+    for (const i in content) {
+        for (const j in content[i]) {
+            csv += content[i][j];
         }
         csv += "\n";
     }
 
-    csv = csv.substring(0,csv.length-1);
+    csv = csv.substring(0, csv.length - 1);
 
+    console.log("csv", csv);
     return csv;
 }
 
-function csv_to_json (leCsv){
-
-    const lines = leCsv.split("\n")
-    const header = lines[0].split(" ; ")
+function csv_to_json(leCsv) {
+    const lines = leCsv.split("\n");
+    const header = lines[0].split(" ; ");
     let content = [];
     let leJson = {};
 
-    for(const l in lines){
-        if (l>0){
-            content[l-1] = lines[l].split(" ; ")
+    for (const l in lines) {
+        if (l > 0) {
+            content[l - 1] = lines[l].split(" ; ");
         }
     }
 
-    for (const c in content){
+    for (const c in content) {
         leJson[c] = {};
-        for (const t in header){
-        leJson[c][header[t]] = content[c][t];
+        for (const t in header) {
+            leJson[c][header[t]] = content[c][t];
         }
     }
 
     return leJson;
 }
 
-console.log(json_to_csv (json_valid))
-console.log("\n\n")
-console.log(csv_to_json(json_to_csv (json_valid)));
+console.log(json_to_csv(json_valid));
+console.log("\n\n");
+console.log("=".repeat(50));
+console.log(csv_to_json(json_to_csv(json_valid)));
+console.log("=".repeat(50));
